694bcd649618cc17a9e1c5ae7cc65639960f7db5
EihabEmadSd <eihabimad@gmail.com>
2020-11-10 13:21:41 +0400

    Initial

---
+++ build/precache-manifest.cf77bdac89bd3669b6d769ce44582222.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e7f96824f1b6a7a9f642f7f36bb9874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "362f8ed8f80a6df29cc0",
+    "url": "/static/css/2.19a0d79e.chunk.css"
+  },
+  {
+    "revision": "76746950b7023812ac85",
+    "url": "/static/css/main.5aa9c5f8.chunk.css"
+  },
+  {
+    "revision": "362f8ed8f80a6df29cc0",
+    "url": "/static/js/2.33f99080.chunk.js"
+  },
+  {
+    "revision": "c4e6a225f62a280eed58f951a5d30044",
+    "url": "/static/js/2.33f99080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263d8741d69c8f608042",
+    "url": "/static/js/3.d8b31c32.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.d8b31c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e1c99114e33c2df5fdf",
+    "url": "/static/js/4.4accdefb.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.4accdefb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a68f5663c334e40b94",
+    "url": "/static/js/5.55da3651.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.55da3651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76746950b7023812ac85",
+    "url": "/static/js/main.3c95db77.chunk.js"
+  },
+  {
+    "revision": "0f2427b9547f211ae174",
+    "url": "/static/js/runtime-main.86da5405.js"
+  }
+]);
